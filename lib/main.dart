9986e666edda9728a29555105f4d40891cb7f840
exactusJava <luizricardo@exactus.com.br>
2021-08-23 13:32:10 -0300

    getData api , future builder, asnc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main(){
+var request =
+    Uri.parse('https://api.hgbrasil.com/finance?format=json&key=f6b73b8f');
+
+void main() async {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -18,12 +25,48 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("\$ Conversor de Moedas \$",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "\$ Conversor de Moedas \$",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: Colors.amber,
-
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case (ConnectionState.none):
+            case (ConnectionState.waiting):
+              return Center(
+                  child: Text(
+                "Carregando dados...",
+                style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                textAlign: TextAlign.center,
+              ));
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                    child: Text(
+                  "Erro ao carregar dados",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ));
+              } else {
+                return Container(
+                  color: Colors.green,
+                );
+              }
+          }
+        },
       ),
     );
   }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return jsonDecode(response.body);
 }
